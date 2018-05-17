7bb42392aab5a6a11b199f20a5da9650c3423afb
Saul Ortigoza <saul.ortigoza@wizeline.com>
2018-05-17 11:13:48 -0500

    using wize pipy

--- docker/publish_install.sh
+++ docker/publish_install.sh
@@ -9,9 +9,9 @@ echo "publish to s3 bucket"
 s3pypi --bucket $BUCKET_NAME --force
 
 echo "install from s3 bucket"
-PIP3_URL=http://$BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com/
+PIP3_URL=http://$BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com/truffleHogWize/
 TRUSTED_HOST=$BUCKET_NAME.s3-website-$AWS_REGION.amazonaws.com
 
-pip3 install truffleHog==2.0.92 -v \
+pip3 install truffleHogWize==2.0.92.1 -v \
   --extra-index-url $PIP3_URL \
   --trusted-host $TRUSTED_HOST
